1a8d0cec0f573ea880672f283ee712bff9b9d762
PequalsNPaspirant <marutimhetre@gmail.com>
2018-12-08 18:39:01 -0500

    template definition of tuple that works on MS Visual Studio

--- src/TemplateMetaprogramming/TemplateMetaProgramming_tuple.h
+++ src/TemplateMetaprogramming/TemplateMetaProgramming_tuple.h
@@ -24,27 +24,29 @@ namespace mm {
 		{
 		}
 
-		template<size_t index> 
+		template<size_t index>
 		struct elementType
 		{
 			typedef typename tuple<Ts...>::template elementType<index - 1>::type type;
 		};
 
 		template<>
-		struct elementType<size_t(0)>
+		struct elementType<0>
 		{
 			typedef T type;
 		};
 
 		template<size_t index>
-		typename tuple<T, Ts...>::template elementType<index>::type getAt(void)
+		typename tuple<T, Ts...>::template elementType<index>::type getAt()
 		//auto getAt()
-		{ 
-			return static_cast<tuple<Ts...>>(*this).getAt<index - 1>();
+		{
+			tuple<Ts...>& ref = static_cast<tuple<Ts...>>(*this);
+			return ref.getAt<index - 1>();
 		}
 
 		template<>
-		typename tuple<T, Ts...>::template elementType<0>::type  getAt<0>(void)
+		typename tuple<T, Ts...>::template elementType<0>::type  getAt<0>()
+		//T getAt<0>()
 		{
 			return tail;
 		}
@@ -52,5 +54,37 @@ namespace mm {
 	protected:
 		T tail;
 	};
+
+	//g++ does not allow explicit template specialization inside class scope, it should be in enclosing namespace scope
+	//template <int index, typename T, typename... Ts>
+	////template<int index>
+	//struct tuple<T, Ts...>::elementType<index>
+	//{
+	//	typedef typename tuple<Ts...>::template elementType<index - 1>::type type;
+	//};
+	
+	//template <typename T, typename... Ts>
+	////template<>
+	//struct tuple<T, Ts...>::elementType<0>
+	//{
+	//	typedef T type;
+	//};
+
+	//g++ does not allow explicit template specialization inside class scope, it should be in enclosing namespace scope
+	//template <typename T, typename... Ts>
+	//template<int index>
+	//typename tuple<T, Ts...>::template elementType<index - 1>::type getAt<index>()
+	//	//auto getAt()
+	//{
+	//	tuple<Ts...>& ref = static_cast<tuple<Ts...>>(*this);
+	//	return ref.getAt<index - 1>();
+	//}
+
+	//template<>
+	//template <typename T, typename... Ts>
+	//typename tuple<T, Ts...>::template elementType<0>::type tuple<T, Ts...>::getAt<0>(void)
+	//{
+	//	return tail;
+	//}
 
 }
