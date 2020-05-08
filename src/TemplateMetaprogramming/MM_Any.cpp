b6662c80d72fb73255f8ad52b8ce6202c9ff455b
PequalsNPaspirant <marutimhetre@gmail.com>
2020-05-08 01:48:52 -0400

    Added class MM_Any

--- src/TemplateMetaprogramming/MM_Any.cpp
+++ src/TemplateMetaprogramming/MM_Any.cpp
@@ -17,17 +17,67 @@ namespace mm {
 		class A {};
 		A a;
 
+		//Test Construction from l-value
 		MM_Any iObj{ i };
 		MM_Any llObj{ ll };
 		MM_Any dObj{ d };
 		MM_Any aObj{ a };
 
+		//Test Construction from r-value
+
+		//Test operator T()
 		int i2;
 		i2 = iObj;
 		i2 = llObj;
 		i2 = dObj;
 		i2 = aObj;
+		//Test function getValue<T>()
+		i2 = iObj.getValue<int>();
+		i2 = llObj.getValue<int>();;
+		i2 = dObj.getValue<int>();;
+		i2 = aObj.getValue<int>();;
 
+		long long ll2;
+		ll2 = iObj;
+		ll2 = llObj;
+		ll2 = dObj;
+		ll2 = aObj;
+		ll2 = iObj.getValue<long long>();
+		ll2 = llObj.getValue<long long>();
+		ll2 = dObj.getValue<long long>();
+		ll2 = aObj.getValue<long long>();
+
+		double d2;
+		d2 = iObj;
+		d2 = llObj;
+		d2 = dObj;
+		d2 = aObj;
+		d2 = iObj.getValue<double>();
+		d2 = llObj.getValue<double>();
+		d2 = dObj.getValue<double>();
+		d2 = aObj.getValue<double>();
+
+		A a2;
+		a2 = iObj;
+		a2 = llObj;
+		a2 = dObj;
+		a2 = aObj;
+		a2 = iObj.getValue<A>();
+		a2 = llObj.getValue<A>();
+		a2 = dObj.getValue<A>();
+		a2 = aObj.getValue<A>();
+
+		//Test copy constructor
+		MM_Any iObj2{ iObj };
+		MM_Any llObj2{ llObj };
+		MM_Any dObj2{ dObj };
+		MM_Any aObj2{ aObj };
+
+		//Test move constructor
+
+		//Test copy assignment
+
+		//Test move assignment
 	}
 
 
--- src/TemplateMetaprogramming/MM_Any.h
+++ src/TemplateMetaprogramming/MM_Any.h
@@ -9,6 +9,7 @@ https://en.cppreference.com/w/cpp/types
 #include <vector>
 #include <map>
 #include <memory>
+#include<type_traits>
 using namespace std;
 
 namespace mm{
@@ -21,36 +22,46 @@ namespace mm{
 		{
 			data_ = make_unique< ConcreteType<T> >(obj);
 		}
-		//template<typename T>
-		//MM_Any(T&& obj)
-		//{
-		//	data_ = make_unique< ConcreteType<T> >(std::move(obj));
-		//}
+		template<typename T>
+		MM_Any(std::remove_reference_t<T>&& obj)
+		{
+			data_ = make_unique< ConcreteType<T> >(std::move(obj));
+		}
 
 		MM_Any(const MM_Any& copy)
 		{
-
+			data_ = copy.data_->clone();
 		}
 
-		//MM_Any(MM_Any&& move)
-		//{
-
-		//}
+		MM_Any(MM_Any&& move)
+		{
+			data_ = std::move(move.data_);
+		}
 
 		MM_Any& operator=(const MM_Any& copy)
 		{
+			if (this == &copy)
+				return *this;
+
+			data_ = copy.data_->clone();
 			return *this;
 		}
 
 		void operator=(MM_Any&& move)
 		{
-
+			data_ = std::move(move.data_);
 		}
 
 		template<typename T>
 		operator T()
 		{
-			return data_->getData<T>();
+			return getValue<T>();
+		}
+
+		template<typename T>
+		T getValue()
+		{
+			return data_->getValue<T>();
 		}
 
 	private:
@@ -58,15 +69,16 @@ namespace mm{
 		{
 		public:
 			template<typename T>
-			T getData()
+			T getValue()
 			{
 				ConcreteType<T>* ct = dynamic_cast<ConcreteType<T>*>(this);
 				if (ct != nullptr)
-					return ct->getConcreteData();
+					return ct->getConcreteValue();
 
 				return T{};
 			}
 
+			virtual unique_ptr<AnyType> clone() = 0;
 			virtual ~AnyType() {}
 		};
 
@@ -78,9 +90,9 @@ namespace mm{
 				: val_{ t }
 			{}
 
-			//ConcreteType(T&& t)
-			//	: val_{ std::move(t) }
-			//{}
+			ConcreteType(std::remove_reference_t<T>&& t)
+				: val_{ std::move(t) }
+			{}
 
 			//template<typename T>
 			//void getData(T& n) override
@@ -88,9 +100,14 @@ namespace mm{
 			//	n = val_;
 			//}
 
-			T getConcreteData()
+			T getConcreteValue()
 			{
 				return val_;
+			}
+
+			unique_ptr<AnyType> clone() override
+			{
+				return make_unique< ConcreteType<T> >(*this);
 			}
 
 		private:
